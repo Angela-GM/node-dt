e52c8038afeb9cc04d665b621822cbefd6d3dc72
Angela GM <angelagm0112@gmail.com>
2025-02-07 16:15:29 +0100

    callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,5 +1,22 @@
 const {emailTemplate} = require('./js-foundation/01-template');
+require('./js-foundation/02-destructuring')
+const {getUserById} = require('./js-foundation/03-callbacks')
+
+
 // import { emailTemplate } from './js-foundation/01-template'
 
 // console.log(templateExports.emailTemplate)
-console.log(emailTemplate);
+// console.log(emailTemplate);
+
+const id = 3;
+
+
+// getUserById(1, (error, user) => {
+getUserById(id, function (error, user) {
+  if (error) {
+        throw new Error( error)
+
+    }
+    console.log({user})
+
+})
